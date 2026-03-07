c6a71b6128a80c9db0fb1eb60f06cb8324e3bd4c
gammingbazaar-bd <julfirahman539@gmail.com>
2026-03-08 00:24:38 +0600

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.codvouchers.com'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.gammingbazaar.com'
 
   return {
     rules: {
